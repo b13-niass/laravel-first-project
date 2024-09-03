6645be87dc37e2a0755562d9e56dc1343b2f8f8a
Cheikh Ibrahima Dieng <barhamadieng66@gmail.com>
2024-09-03 06:10:36 +0000

    Ajoute du scope Filter à Article

--- app/Models/Article.php
+++ app/Models/Article.php
@@ -2,9 +2,12 @@
 
 namespace App\Models;
 
+use App\Filters\QuantityFilter;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Builder;
+use Spatie\QueryBuilder\QueryBuilder;
 
 class Article extends Model
 {
@@ -29,5 +32,27 @@ class Article extends Model
         return $this->belongsToMany(Dette::class, 'article_dette')
             ->withPivot('qteVente', 'prixVente')
             ->withTimestamps();
+    }
+
+    /**
+     * Scope a query to apply custom filters.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param \Illuminate\Http\Request $request
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeFilter(Builder $query, $request)
+    {
+        // Apply the 'disponible' filter using the custom QuantityFilter
+        if ($request->has('disponible')) {
+            $disponible = $request->query('disponible');
+            $query = (new QuantityFilter())($query, $disponible, 'disponible');
+        }
+
+        // Use Spatie's QueryBuilder to handle other allowed filters
+        $query = QueryBuilder::for($query)
+            ->allowedFilters(['libelle']);
+
+        return $query;
     }
 }
